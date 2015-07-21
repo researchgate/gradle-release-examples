adc2f27e278736701dadf0f0652f34e0a1577bef
Daniel Tschinder <code@tschinder.de>
2015-07-21 22:26:19 +0200

    Add simple single project

---
+++ single-project/src/main/java/net/researchgate/release/examples/singleproject/Main.java
@@ -0,0 +1,16 @@
+/*
+ * This file is part of the gradle-release-examples.
+ *
+ * (c) ResearchGate GmbH
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+package net.researchgate.release.examples.singleproject;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ single-project/src/test/java/net/researchgate/release/examples/singleproject/MainTest.java
@@ -0,0 +1,39 @@
+/*
+ * This file is part of the gradle-release-examples.
+ *
+ * (c) ResearchGate GmbH
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+package net.researchgate.release.examples.singleproject;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class MainTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
+
+    @Test
+    public void testMain() {
+        Main.main(new String[]{});
+        Assert.assertEquals("Hello world!", outContent.toString().trim());
+    }
+}
